7f269caafe42e6896c801a8a9139b38ee2af6206
Grace Bong <gracebong.park@gmail.com>
2021-07-08 23:21:16 -0400

    initial setup

--- src/Components/BookmarkDetails.js
+++ src/Components/BookmarkDetails.js
@@ -1,9 +1,34 @@
 import { useState } from "react";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 function BookmarkDetails() {
   const [bookmark, setBookmark] = useState([]);
-  return <article></article>;
+  return <article>
+    <h3>{true ? <span>⭐️</span> : null} bookmark.name</h3>
+    <h5>
+        <span>
+          <a href={bookmark.url}>bookmark.name</a>
+        </span>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; bookmark.url
+      </h5>
+      <h6>bookmark.category</h6>
+      <p>bookmark.description</p>
+      <div className="showNavigation">
+      <div>
+        <Link to={`/bookmarks`}>
+          <button>Back</button>
+        </Link>
+      </div>
+      <div>
+        <Link to={`/bookmarks/id/edit`}>
+          <button>Edit</button>
+        </Link>
+      </div>
+      <div>
+        <button>Delete</button>
+      </div>
+    </div>
+  </article>;
 }
 
 export default withRouter(BookmarkDetails);
